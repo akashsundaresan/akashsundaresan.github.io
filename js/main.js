ec638cf1646646c7e1826d432596ee5fa8f8f12f
Akash S M <akashsm62@gmail.com>
2021-11-27 17:43:45 +0530

    Minor fix

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
         if ($(this).scrollTop() > 200) {
             $('.back-to-top').fadeIn('slow');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $('.back-to-top').fadeOut('slow');  
         }
     });
     $('.back-to-top').click(function () {
